08bcf268696c15e3df747f1468490dd5ffdb7447
devadh21 <devadh21@gmail.com>
2024-10-03 16:31:26 +0000

    Update page.tsx  disbled getProducts function

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,27 @@ import Services from "@/components/home/Services";
 import Newslatter from "@/components/home/Newslatter";
 
 
-async function getProducts() {  
-  const res = await fetch(process.env.URL_SITE+"/api/products/honeys",{ 
-    // cache: 'no-store'
-    headers:{
-      'authorization': '1234567890abcdefghijklmnopqrstuvwxyz',
-    }
-  });  
-  if (!res.ok) throw new Error(await res.text()); 
-  revalidatePath("/");
-  return res.json(); 
-}
+// async function getProducts() {  
+//   // const res = await fetch(process.env.URL_SITE+"/api/products/honeys",{ 
+//   const res = await fetch("http://localhost:3000/api/products/honeys",{ 
+//     // cache: 'no-store'
+//     headers:{
+//       'authorization': '1234567890abcdefghijklmnopqrstuvwxyz',
+//     }
+//   });   
+//   if (!res.ok) throw new Error(await res.text()); 
+//   revalidatePath("/");
+//   return res.json(); 
+// }
 
 export default async  function Home() {
   // Fetching products data from the server.
-  const data = await getProducts(); 
+  //const data = await getProducts(); 
 
   return (
     <main className="full-screen">     
       <Hero />
-      <Products products={data.data} />
+      {/* <Products products={data.data} /> */}
       <About />
       <WhyUs/>      
       <Services/>      
